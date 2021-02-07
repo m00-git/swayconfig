36896aeec2c4f83ba626965add46501201e3abf7
m00-git <c0wcl4n@outlook.com>
2021-02-07 17:19:04 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ mkdir .config/waybar
 mv swayconfig .config/sway/config
 mv waybarconfig .config/waybar/config
 mv waybarstyle.css .config/waybar/style.css
+mv bg.png .config/sway/bg.png
 #eventually add .bashrc and .vimrc

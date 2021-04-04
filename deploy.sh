762b8e6d0a2fb16885c2f7627b1f5fbe6dcc0963
m00-git <c0wcl4n@outlook.com>
2021-04-03 23:54:45 -0400

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-mkdir ~/.config
-mkdir ~/.config/sway
-mkdir ~/.config/waybar
-mv swayconfig ~/.config/sway/config
-mv waybarconfig ~/.config/waybar/config
-mv waybarstyle.css ~/.config/waybar/style.css
-mv bg.png ~/.config/sway/bg.png
-mv .vimrc ~/.vimrc
-cat .bashrc >> ~/.bashrc

bd4680c7b36ba4d0fa8330c7440d49fdb515b06d
m00-git <c0wcl4n@outlook.com>
2021-04-04 21:56:23 -0500

    Create finalconfig.sh

---
+++ finalconfig.sh
@@ -0,0 +1,11 @@
+cd
+rm .vimrc
+wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.vimrc
+rm .bashrc
+wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.bashrc
+mkdir ~/.config/sway; cd ~/.config/sway
+wget https://github.com/m00-git/swayconfig/raw/main/bg.png
+wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.bashrc
+wget https://raw.githubusercontent.com/m00-git/swayconfig/main/waybarconfig
+wget https://raw.githubusercontent.com/m00-git/swayconfig/main/swayconfig
+wget https://raw.githubusercontent.com/m00-git/swayconfig/main/waybarstyle.css

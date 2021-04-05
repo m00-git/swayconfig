50810292f90876fff9441db411591a0d0b366912
m00-git <c0wcl4n@outlook.com>
2021-04-04 22:00:13 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -19,15 +19,6 @@ echo "permit $USER" > /etc/doas.conf
 pacman -Rns sudo
 
 ### Rice me out ###
-su $USER
-cd
-rm .vimrc
-wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.vimrc
-rm .bashrc
-wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.bashrc
-mkdir ~/.config/sway; cd ~/.config/sway
-wget https://github.com/m00-git/swayconfig/raw/main/bg.png
-wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.bashrc
-wget https://raw.githubusercontent.com/m00-git/swayconfig/main/waybarconfig
-wget https://raw.githubusercontent.com/m00-git/swayconfig/main/swayconfig
-wget https://raw.githubusercontent.com/m00-git/swayconfig/main/waybarstyle.css
+echo "You will now be entering your user, run bash finalconfig.sh"
+wget -o /home/$USER/finalconfig.sh https://raw.githubusercontent.com/m00-git/install-artix/main/finalconfig.sh
+su $USER 

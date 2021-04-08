655f72c12ada97116a4d22fc10e29b9605b06c9c
m00-git <c0wcl4n@outlook.com>
2021-04-07 20:57:57 -0400

    Update finalconfig.sh

--- finalconfig.sh
+++ finalconfig.sh
@@ -1,3 +1,5 @@
+echo "Whos your home user again lol"
+read USER
 cd
 rm .vimrc
 wget https://raw.githubusercontent.com/m00-git/swayconfig/main/.vimrc
@@ -14,10 +16,14 @@ wget https://raw.githubusercontent.com/m00-git/swayconfig/main/swayconfig
 wget https://raw.githubusercontent.com/m00-git/swayconfig/main/waybarstyle.css
 
 # my needed packages
-pacman -S pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu nftables nftables-runit openvpn
+pacman -S pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu qemu-guest-agent-runit nftables nftables-runit openvpn
 trizen -S fedora-firefox-wayland-bin
 
 doas ln -s /etc/runit/sv/libvirtd /run/runit/service
+doas ln -s /etc/runit/sv/qemu-guest-agent /run/runit/service
+doas sv up libvrtd
+doas sv up qemu-guest-agent
+doas usermod --append --groups $USER
 cd /etc/openvpn
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/update-resolv-conf
 doas chmod +x update-resolv-conf

95b4ab75c59d45ea459b9e29c85b8dd55d4fc01c
m00-git <c0wcl4n@outlook.com>
2021-04-09 14:43:10 -0400

    Update finalconfig.sh

--- finalconfig.sh
+++ finalconfig.sh
@@ -16,11 +16,12 @@ wget https://raw.githubusercontent.com/m00-git/swayconfig/main/swayconfig
 wget https://raw.githubusercontent.com/m00-git/swayconfig/main/waybarstyle.css
 
 # my needed packages
-pacman -S pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu qemu-guest-agent-runit nftables nftables-runit openvpn otf-font-awesome transmission-gtk
+pacman -S pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu qemu-guest-agent-runit nftables nftables-runit openvpn otf-font-awesome transmission-gtk openntpd openntpd-runit
 trizen -S fedora-firefox-wayland-bin
 
 doas ln -s /etc/runit/sv/libvirtd /run/runit/service
 doas ln -s /etc/runit/sv/qemu-guest-agent /run/runit/service
+doas ln -s /etc/runit/sv/openntpd
 doas sv up libvrtd
 doas sv up qemu-guest-agent
 doas usermod --append --groups $USER

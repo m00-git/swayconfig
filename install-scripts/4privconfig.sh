dcba8081bea87b616caf77734a602914f2d4b569
m00-git <c0wcl4n@outlook.com>
2021-04-24 15:38:18 -0400

    Update 4privconfig.sh

--- install-scripts/4privconfig.sh
+++ install-scripts/4privconfig.sh
@@ -1,26 +1,27 @@
 echo "whos your user"
 read USER
-pacman -S neofetch doas trizen tmux termite wget sway waybar pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu bridge-utils qemu-guest-agent-runit nftables nftables-runit openvpn otf-font-awesome transmission-gtk openntpd openntpd-runit
+sudo pacman -S neofetch doas trizen tmux termite wget sway waybar pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu bridge-utils qemu-guest-agent-runit nftables nftables-runit openvpn otf-font-awesome transmission-gtk openntpd openntpd-runit
 
-ln -s /etc/runit/sv/libvirtd /run/runit/service
-sv up libvrtd
-ln -s /etc/runit/sv/qemu-guest-agent /run/runit/service
-sv up qemu-guest-agent
-ln -s /etc/runit/sv/openntpd /run/runit/service
-sv up openntpd
-ln -s /etc/runit/sv/virtlockd /run/runit/service
-sv up virtlockd
-ln -s /etc/runit/virtlogd /run/runit/service
-sv up virtlogd
+sudo ln -s /etc/runit/sv/libvirtd /run/runit/service
+sudo sv up libvrtd
+sudo ln -s /etc/runit/sv/qemu-guest-agent /run/runit/service
+sudo sv up qemu-guest-agent
+sudo ln -s /etc/runit/sv/openntpd /run/runit/service
+sudo sv up openntpd
+sudo ln -s /etc/runit/sv/virtlockd /run/runit/service
+sudo sv up virtlockd
+sudo ln -s /etc/runit/virtlogd /run/runit/service
+sudo sv up virtlogd
 
-echo "permit $USER" > /etc/doas.conf
-pacman -Rns sudo
-usermod --append --groups $USER
+sudo echo "permit $USER" > /etc/doas.conf
+sudo pacman -Rns sudo
 
-mkdir /etc/openvpn
+sudo usermod --append --groups $USER
+
+sudo mkdir /etc/openvpn
 cd /etc/openvpn
-wget https://raw.githubusercontent.com/m00-git/install-artix/main/sysfiles/update-resolv-conf
-chmod +x update-resolv-conf
+sudo wget https://raw.githubusercontent.com/m00-git/install-artix/main/sysfiles/update-resolv-conf
+sudo chmod +x update-resolv-conf
 
 cd /home/$USER
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/install-scripts/5finalconfig.sh

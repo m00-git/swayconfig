ec565d8d3c80d0f6a5566f2da749bc94c6e1179a
m00-git <c0wcl4n@outlook.com>
2021-04-24 03:02:47 -0400

    Update 3config.sh

--- install-scripts/3config.sh
+++ install-scripts/3config.sh
@@ -14,30 +14,9 @@ chmod -R g-rwx,o-rwx /boot
 ### Base System Setup ###
 ln -s /etc/runit/sv/NetworkManager /run/runit/service
 sv up NetworkManager
-pacman -S neofetch doas trizen tmux termite wget sway waybar pulseaudio pavucontrol fv vlc virt-manager libvirt-runit qemu bridge-utils qemu-guest-agent-runit nftables nftables-runit openvpn otf-font-awesome transmission-gtk openntpd openntpd-runit
-
-ln -s /etc/runit/sv/libvirtd /run/runit/service
-sv up libvrtd
-ln -s /etc/runit/sv/qemu-guest-agent /run/runit/service
-sv up qemu-guest-agent
-ln -s /etc/runit/sv/openntpd /run/runit/service
-sv up openntpd
-ln -s /etc/runit/sv/virtlockd /run/runit/service
-sv up virtlockd
-ln -s /etc/runit/virtlogd /run/runit/service
-sv up virtlogd
-
-echo "permit $USER" > /etc/doas.conf
-pacman -Rns sudo
-usermod --append --groups $USER
-
-mkdir /etc/openvpn
-cd /etc/openvpn
-wget https://raw.githubusercontent.com/m00-git/install-artix/main/sysfiles/update-resolv-conf
-chmod +x update-resolv-conf
 
 ### Rice me out ###
 cd /home/$USER
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/install-scripts/4finalconfig.sh
-echo "You will now be entering your user, run bash 4finalconfig.sh from your home dir"
+echo "You will now be entering your user, run doas bash 4privconfig.sh from your home dir"
 su $USER 

bdb2e92eea98d0099f2fc6463d609abf42af1fca
m00-git <c0wcl4n@outlook.com>
2021-05-12 05:14:49 -0400

    Update 2chrooted.sh

--- install-scripts/2chrooted.sh
+++ install-scripts/2chrooted.sh
@@ -10,9 +10,18 @@ echo "Set your root password"
 passwd # set root password
 echo "Name of your user?"
 read USER
+
+echo "USER=$USER" >> /vars
 useradd -m -G wheel $USER
+
+userans=t
+while [ "$userans" != y ]
+do
 echo "Set your user password"
 passwd $USER # set user password
+echo "Was password set successfully? y/n"
+read userans
+done
 
 rm /etc/mkinitcpio.conf; cd /etc
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/sysfiles/mkinitcpio.conf
@@ -20,8 +29,9 @@ cd
 mkinitcpio -p linux
 
 fdisk -l | grep /dev
-echo "Input again the drive you have chosen to install Artix on. Ex: sda"
-read DRIVE
+# This part should already be known as we appended this script to /vars
+# echo "Input again the drive you have chosen to install Artix on. Ex: sda"
+# read DRIVE
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/sysfiles/grub-top
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/sysfiles/grub-bottom
 echo GRUB_CMDLINE_LINUX=\"cryptdevice=/dev/$DRIVE\1:artix:allow-discards\" >> grub-top
@@ -33,10 +43,14 @@ rm grub-*
 grub-install --target=i386-pc /dev/$DRIVE
 grub-mkconfig -o /boot/grub/grub.cfg
 
+cp /vars tmpvars
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/install-scripts/3postchroot.sh
-chmod +x 3postchroot.sh
+cat 3postchroot.sh >> tmpvars
+mv tmpvars /root/3postchroot.sh
+chmod +x /root/3postchroot.sh
 
-echo "You must now manually enter the following commands: after reboot, as root run bash 3postchroot.sh"
+echo "You must now manually enter the following commands"
 echo "exit"
 echo "umount -R /mnt"
 echo "reboot"
+echo "After the reboot, login as root and then run bash 3postchroot.sh"

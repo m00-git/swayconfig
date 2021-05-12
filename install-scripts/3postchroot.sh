3f3d673e2a27306c8127b22058e06f2d7b957080
m00-git <c0wcl4n@outlook.com>
2021-05-12 05:17:05 -0400

    Update 3postchroot.sh

--- install-scripts/3postchroot.sh
+++ install-scripts/3postchroot.sh
@@ -1,8 +1,10 @@
 fdisk -l | grep /dev
-echo "Input again the drive you are installing artix to:"
-read DRIVE
-echo "Input again the name of the user you made earlier:"
-read USER
+# This should all be taken care of
+# echo "Input again the drive you are installing artix to:"
+# read DRIVE
+# echo "Input again the name of the user you made earlier:"
+# read USER
+
 # Create keyfile so you dont have to enter password twice on boot
 dd bs=512 count=4 if=/dev/urandom of=/crypto_keyfile.bin
 cryptsetup luksAddKey /dev/$DRIVE\1 /crypto_keyfile.bin
@@ -18,6 +20,9 @@ echo "$USER ALL=(ALL) ALL" >> /etc/sudoers
 
 ### Rice me out ###
 cd /home/$USER
+cp /vars tmpvars
 wget https://raw.githubusercontent.com/m00-git/install-artix/main/install-scripts/4privconfig.sh
+cat 4privconfig.sh >> tmpvars
+mv tmpvars 4privconfig.sh
 echo "You will now be entering your user, run bash 4privconfig.sh from your home dir"
 su $USER 

67049b2dc71d023cfe59e542ea433f30c5a88af2
m00-git <c0wcl4n@outlook.com>
2021-05-12 04:53:02 -0400

    Update 1prechroot.sh

--- install-scripts/1prechroot.sh
+++ install-scripts/1prechroot.sh
@@ -1,11 +1,16 @@
 pacman -Sy
 pacman -S parted
 fdisk -l | grep /dev
+
 echo "Input the drive you wish to install Artix to. Ex: sda"
 read DRIVE
+
+echo "DRIVE=$DRIVE" >> vars
+
 parted -s /dev/$DRIVE mklabel msdos
 parted -s /dev/$DRIVE mkpart primary 2048s 100%
 cryptsetup --type luks1 --cipher aes-xts-plain64 --hash sha512 --use-random --verify-passphrase luksFormat /dev/$DRIVE\1
+
 # Create filesystems and subvolumes
 cryptsetup open /dev/$DRIVE\1 artix
 mkfs -t btrfs --force -L artix /dev/mapper/artix
@@ -22,8 +27,20 @@ o_btrfs=$o,compress=lzo,ssd,noatime
 mount -o compress=lzo,subvol=@,$o_btrfs /dev/mapper/artix /mnt
 mount -o compress=lzo,subvol=@home,$o_btrfs /dev/mapper/artix /mnt/home
 mount -o compress=lzo,subvol=@snapshots,$o_btrfs /dev/mapper/artix /mnt/.snapshots
+
+echo "Is your CPU AMD or INTEL?"
+read CPU
+if [ "$CPU" == AMD ]
+then
 # Install base system
 basestrap /mnt base base-devel runit elogind-runit linux-hardened linux-hardened-headers usbctl linux-firmware wget vim amd-ucode btrfs-progs grub snapper mlocate networkmanager networkmanager-runit network-manager-applet dosfstools cryptsetup doas
+if [ "$CPU" == INTEL ]
+then
+basestrap /mnt base base-devel runit elogind-runit linux-hardened linux-hardened-headers usbctl linux-firmware wget vim intel-ucode btrfs-progs grub snapper mlocate networkmanager networkmanager-runit network-manager-applet dosfstools cryptsetup doas
+else
+echo "Input either AMD or INTEL, you inputted $CPU"
+fi
+
 # Generate fstab
 fstabgen -U /mnt >> /mnt/etc/fstab
 cd /mnt

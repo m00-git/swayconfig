24a251947dbd58258821c2f1ef891d7ebf8a9dca
m00-git <c0wcl4n@outlook.com>
2021-06-07 22:49:23 -0400

    Update 5finalconfig.sh

--- install-scripts/5finalconfig.sh
+++ install-scripts/5finalconfig.sh
@@ -3,6 +3,7 @@ wget https://github.com/m00-git/install-artix/raw/main/BlockZone.ttf
 mkdir -p ~/.local/share/fonts 
 mv BlockZone.ttf ~/.local/share/fonts
 fc-cache -f -v 
+
 # Rest of configs
 mv .config/* ~/.config/
 mv .vim ~/.vim

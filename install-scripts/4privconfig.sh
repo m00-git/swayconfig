45dde28fb811b530421ca18edd9d304de72ecba8
m00-git <c0wcl4n@outlook.com>
2021-06-07 22:34:08 -0400

    Update 4privconfig.sh

--- install-scripts/4privconfig.sh
+++ install-scripts/4privconfig.sh
@@ -1,14 +1,17 @@
 # Should all be taken care of
 # echo "whos your user"
 # read USER
+sudo pacman -S artix-archlinux-support archlinux-mirrorlist
+
+
 sudo pacman -S neofetch doas trizen tmux termite wget rofi sway waybar pulseaudio pavucontrol vlc virt-manager libvirt-runit qemu bridge-utils qemu-guest-agent-runit nftables nftables-runit openvpn otf-font-awesome transmission-gtk chrony chrony-runit wl-clipboard grim slurp swappy xorg-xwayland ttf-nerd-fonts-symbols-mono
 
 sudo ln -s /etc/runit/sv/libvirtd /run/runit/service
-sudo sv up libvrtd
+sudo sv up libvirtd
 sudo ln -s /etc/runit/sv/qemu-guest-agent /run/runit/service
 sudo sv up qemu-guest-agent
-sudo ln -s /etc/runit/sv/openntpd /run/runit/service
-sudo sv up openntpd
+sudo ln -s /etc/runit/sv/chrony /run/runit/service
+sudo sv up chrony
 sudo ln -s /etc/runit/sv/virtlockd /run/runit/service
 sudo sv up virtlockd
 sudo ln -s /etc/runit/virtlogd /run/runit/service
